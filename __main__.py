a929ac0e1274e356e325a81295304f0813d9568c
leegee1080 <mitchell.lee.greene@gmail.com>
2021-03-04 16:41:38 -0500

    started work on the change password part of the script, finished work on the default code exe part, and the custom code exe part. The script is ready to use.

--- __main__.py
+++ __main__.py
@@ -1,11 +1,6 @@
-import datetime
-import getpass
-import re
-import os
-import time
-import select
+import datetime, getpass, re, os, socket
 
-version = 0.4
+version = 0.9
 
 print(f"\n\nYou are using version {version} of the 3DprinterOS Driver Install Script by Mitchell Greene.")
 usage_warning = input("\tBy pressing 'enter' you accept that useage of this program is at your own risk.\n\tIf you do not accept these terms, close this program by pressing 'CTRL-C'.\n\n")
@@ -57,6 +52,18 @@ def Command_Constructor(code_template, first_code, second_code):
     command= re.sub("!TOKEN!", second_code, command)
     return command
 
+def Check_3dPos_Client_Port(ip_to_check):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result = sock.connect_ex((ip_to_check, 8008))
+    if result == 0:
+        print("3dPOS is running.")
+        sock.close()
+        return f"3dPOS on IP({ip_to_check}) is running.\n"
+    else:
+        print("3dPOS is not running.")
+        sock.close()
+        return f"!!!!3dPOS on IP({ip_to_check}) is unreachable!!!!\n"
+    
 def End_Script(new_log_text):
     current_time = datetime.datetime.now()
     log_folder = r"logs\log_"
@@ -65,7 +72,18 @@ def End_Script(new_log_text):
          log_file.write(new_log_text)
     exit
 
+
+
 def main():
+    use_default_codes = False
+    driver_codes = []
+    ip_addresses = []
+    code_pairs = []
+    template_code = ""
+
+    #declare the list that will have the ip addresses combined with the code pairs
+    complete_pairs = []
+
     current_time = datetime.datetime.now()
     #start log file (create new file), append date and time
     log_text = f"----3DprinterOS Driver Install----\nLog started on {current_time.strftime('%H:%M:%S on %A, %B the %dth, %Y')}\n"
@@ -74,31 +92,45 @@ def main():
     print("What is the password for the root user?")
     user_password = getpass.getpass()
 
-
 # ====================================================
     #OPTIONAL!!
     #ask user if they would like to batch change pw for root user
     #ask user if they would like to run custom code
-    #if yes then ask if user would like to run custom code with TScode and token filled in.
-    #otherwise run default code
-    try:
-        default_code_file_folder = r"src\default-code.txt"
-        with open(default_code_file_folder, "r") as default_code_file:
-            print("Found default code file.")
-            default_terminal_line = default_code_file.read()
-    except IOError:
-        print("File named 'default-code.txt' not found or has been changed.")
-        exit()
+    while(True):
+        playeranswer = input("What would you like to do? \n (Type 'pw' change password, 'rc' for run custom code, or 'di' 3dPOS driver install.)")
+        if(playeranswer == "pw"):
+            print("Enter the password you would like to apply.")
+            new_user_password1 = getpass.getpass()
+            print("Re-Enter the password you would like to apply.")
+            new_user_password2 = getpass.getpass()
+            if(new_user_password1 == new_user_password2):
+                new_user_password = new_user_password1
+                template_code = "passwd"
+                break
+            else:
+                print("The passwords you entered do not match. Returning to main menu.")
+                continue
+        if(playeranswer == "rc"):
+            template_code = input("What is the code you would like to use?")
+            print(f"The code to be used on each IP in the 'ip_addresses' file is: {template_code}")
+            input("Press enter to confirm.")
+            break
+        if(playeranswer == "di"):
+            try:
+                default_code_file_folder = r"src\default-code.txt"
+                with open(default_code_file_folder, "r") as default_code_file:
+                    print("Found default code file.")
+                    template_code = default_code_file.read()
+                    use_default_codes = True
+            except IOError:
+                print("File named 'default-code.txt' not found or has been changed.")
+                exit()
+            break
+        else:
+            print("Enter a valid response.")
+    #overwrite template_code
+
 #===================================================
-
-    #declare 3 var to use later
-    driver_codes = []
-    ip_addresses = []
-    code_pairs = []
-    template_code = default_terminal_line
-
-    #declare the list that will have the ip addresses combined with the code pairs
-    complete_pairs = []
 
     #open ip file
     #start loop until ip file has no more lines
@@ -147,30 +179,35 @@ def main():
                 line = line.rstrip('\n')
                 if(line != "" and line != None):
                     driver_codes.append(line)
-            # codes_file.seek(0, 0)
-            # codes_file.write("")
-            # codes_file.truncate(0)
+            codes_file.seek(0, 0)
+            codes_file.write("")
+            codes_file.truncate(0)
     except IOError:
         print("File named 'driver-codes.txt' not found.")
         End_Script(log_text)
 
-    #create the code pair tuples
-    print("-------Creating code pairs.-------")
-    for code in driver_codes:
-        print(f"Combining: {code}.")
-        if(Return_TS(code) != "" and Return_Token(code) != ""):
-            code_pairs.append((Return_TS(code), Return_Token(code)))
-        else:
-            continue
+    if(use_default_codes):
+        #create the code pair tuples
+        print("-------Creating code pairs.-------")
+        for code in driver_codes:
+            print(f"Combining: {code}.")
+            if(Return_TS(code) != "" and Return_Token(code) != ""):
+                code_pairs.append((Return_TS(code), Return_Token(code)))
+            else:
+                continue
 
-    #combine all the vars together and check if there is enough codes to go around
-    for index, ip in enumerate(ip_addresses):
-        print(f"Combining: ({ip}).")
-        try:
-            temp_tuple = code_pairs[index]
-            complete_pairs.append((ip,temp_tuple[0],temp_tuple[1]))
-        except IndexError:
-            log_text += f"!!!!{ip} does not have a code pair!!!!\n"
+        #combine all the vars together and check if there is enough codes to go around
+        for index, ip in enumerate(ip_addresses):
+            print(f"Combining: ({ip}).")
+            try:
+                temp_tuple = code_pairs[index]
+                complete_pairs.append((ip,temp_tuple[0],temp_tuple[1]))
+            except IndexError:
+                log_text += f"!!!!{ip} does not have a code pair!!!!\n"
+    else:
+        for ip in ip_addresses:
+            print(f"Combining: ({ip}).")
+            complete_pairs.append((ip,template_code,None))
     print("-------Finished creating code pairs.-------")
 
 
@@ -183,18 +220,24 @@ def main():
             new_client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
             try:
                 new_client.connect(pair[0], port=22, username='root', password=user_password)
-                constructed_command = Command_Constructor(template_code, pair[1], pair[2])
-                print(f"Contstructed code to execute on IP {pair[0]}: {constructed_command}")
-                stdin, stdout, stderr = new_client.exec_command(constructed_command)
-                while not stdout.channel.exit_status_ready():
-                    time.sleep(0.1)
-                     # Only print data if there is data to read in the channel
-                    if stdout.channel.recv_ready():
-                        rl, wl, xl = select.select([stdout.channel], [], [], 0.0)
-                        if len(rl) > 0:
-                        # Print data from stdout
-                            print(stdout.channel.recv(1024))
-                # stdin, stdout, stderr = new_client.exec_command("ip interface brief")
+
+                if(use_default_codes):
+                    constructed_command = Command_Constructor(template_code, pair[1], pair[2])
+                else:
+                    constructed_command = template_code
+
+                print(f"Constructed code to execute on IP {pair[0]}: {constructed_command}")
+                log_text += f"Constructed code to execute on IP {pair[0]}: {constructed_command}\n"
+                ssh_stdin, ssh_stdout, ssh_stderr = new_client.exec_command(constructed_command)
+
+                output = ssh_stdout.read() #Reading output of the executed command
+                print("output:", output) 
+                log_text += f"---constructed code output-----> {output}\n"
+
+                error = ssh_stderr.read() #Reading the error stream of the executed command
+                print("err", error, len(error))
+                log_text += f"---constructed code error-----> {error, len(error)}\n\n"
+
                 new_client.close()
             except (TimeoutError, paramiko.ssh_exception.AuthenticationException) as exceptions:
                 log_text += f"!!!!SSH error on the code ex. stage: {exceptions} on IP: {pair[0]}!!!!\n"
@@ -203,6 +246,9 @@ def main():
     else:
         print("No code pairs to execute.")
         log_text += "No code pairs to execute.\n"
+
+    for ip in ip_addresses:
+        log_text += Check_3dPos_Client_Port(ip)
 
     #if there are unused driver codes ----> append them to a file called 'unused-codes'
     #append log with time stamp and number of codes ran
